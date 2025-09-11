5accfe1d14a928ee1e7aae76344d1047de15b793
santo0 <lemarti3472@gmail.com>
2025-09-11 15:04:18 +0200

    finishing ticket_v1

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,70 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    product_name: String,
+    quantity: u16,
+    unit_price: u16,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u16, unit_price: u16) -> Order {
+        validate_product_name(&product_name);
+        validate_quantity(&quantity);
+        validate_unit_price(&unit_price);
+        Order {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+    pub fn total(&self) -> u16 {
+        self.quantity * self.unit_price
+    }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> &u16 {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &u16 {
+        &self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, product_name: String) {
+        self.product_name = product_name
+    }
+
+    pub fn set_quantity(&mut self, quantity: u16) {
+        self.quantity = quantity
+    }
+
+    pub fn set_unit_price(&mut self, unit_price: u16) {
+        self.unit_price = unit_price
+    }
+}
+
+pub(crate) fn validate_product_name(product_name: &String) {
+    if product_name.is_empty() {
+        panic!("Error")
+    }
+    if product_name.len() > 300 {
+        panic!("Error")
+    }
+}
+
+pub(crate) fn validate_quantity(quantity: &u16) {
+    if *quantity <= 0 {
+        panic!("Error")
+    }
+}
+
+pub(crate) fn validate_unit_price(unit_price: &u16) {
+    if *unit_price <= 0 {
+        panic!("Error")
+    }
+}
